d669f0f511e666660e576bde140855a8709c2a08
Penguen Umut <uuisik@gmail.com>
2023-11-12 00:33:29 +0300

    scss module

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import { MyCounter } from 'upower'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { MyCounter4 } from "upower";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <div style={{ width: '600px', margin: '20px auto' }}>
+    <div style={{ width: "600px", margin: "20px auto" }}>
       <div>
         <h3>Default counter</h3>
-        <MyCounter />
+        <MyCounter4 />
       </div>
       <hr />
       <div>
         <h3>Counter with predefined value</h3>
-        <MyCounter value={5} />
+        <MyCounter4 value={5} />
       </div>
     </div>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
